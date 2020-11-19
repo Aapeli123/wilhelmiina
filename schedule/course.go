340e4fac822544559b4bfb63eae0310ad2d69e08
Capibara123 <aaposharju@gmail.com>
2020-11-19 10:02:16 +0200

    kurssimetodeja implementoitu

--- schedule/course.go
+++ schedule/course.go
@@ -6,6 +6,7 @@ import (
 	"wilhelmiina/database"
 	"wilhelmiina/user"
 
+	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -17,15 +18,60 @@ type Course struct {
 	NameShort     string
 	Description   string
 	CourseSubject Subject
+	CourseID      string
 }
 
 // Group reprsents a group of students that has a subject
 type Group struct {
-	Course   Course
+	CourseID string
 	Name     string
 	Position int8
-	Teacher  user.Teacher
+	Teacher  user.User
 	Students []user.User
+	GroupID  string
+}
+
+// AddGroup creates a new group and saves it to database
+func AddGroup(c Course, teacher user.User, position int8) (Group, error) {
+	prevGroups, err := GetGroupsForCourse(c.CourseID)
+	if err != nil {
+		return Group{}, err
+	}
+	amountOfGroups := len(prevGroups)
+	groupName := fmt.Sprintf("%s.%d", c.NameShort, amountOfGroups+1)
+	group := Group{
+		Name:     groupName,
+		GroupID:  uuid.New().String(),
+		CourseID: c.CourseID,
+		Position: position,
+		Teacher:  teacher,
+		Students: []user.User{},
+	}
+
+	collection := database.DbClient.Database("test").Collection("groups")
+	_, err = collection.InsertOne(context.TODO(), group)
+	if err != nil {
+		return Group{}, err
+	}
+	return group, nil
+}
+
+// GetGroupsForCourse gets all groups for specific course
+func GetGroupsForCourse(CourseID string) ([]Group, error) {
+	filter := bson.M{
+		"courseid": CourseID,
+	}
+	cur, err := database.DbClient.Database("test").Collection("groups").Find(context.TODO(), filter)
+	if err != nil {
+		return nil, err
+	}
+	groups := []Group{}
+	for cur.Next(context.TODO()) {
+		var elem Group
+		cur.Decode(&elem)
+		groups = append(groups, elem)
+	}
+	return groups, nil
 }
 
 // AddCourse adds a new course and saves it to the database
@@ -36,6 +82,7 @@ func AddCourse(name string, desc string, number int, subject Subject) (Course, e
 		CourseSubject: subject,
 		Number:        number,
 		NameShort:     fmt.Sprintf("%s%d", subject.Name, number),
+		CourseID:      uuid.New().String(),
 	}
 	res, err := doesCourseExist(course.NameShort)
 	if err != nil {

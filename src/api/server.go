3df498fa022b20a07d0617a0a06d7133f5793cfa
Capibara123 <aaposharju@gmail.com>
2020-11-21 22:05:45 +0200

    Started writing http api

--- src/api/server.go
+++ src/api/server.go
@@ -1,6 +1,28 @@
 package api
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
 // StartServer starts the http server which will serve the api
 func StartServer(port int) {
+	r := gin.Default()
 
+	r.GET("/ws", websocketHandle)
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func websocketHandle(c *gin.Context) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return
+	}
 }
--- src/go.mod
+++ src/go.mod
@@ -3,7 +3,9 @@ module wilhelmiina
 go 1.15
 
 require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/google/uuid v1.1.2
+	github.com/gorilla/websocket v1.4.2 // indirect
 	go.mongodb.org/mongo-driver v1.4.3
 	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9
 )
